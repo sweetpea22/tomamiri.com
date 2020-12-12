64bc2f88b0b1aa657eb51727f80d7580a0ffa476
sweetpea22 <cindychau24@me.com>
2020-12-12 12:46:24 -0500

    Remove unnecessary navbar stuff

--- src/Components/Modules/Navbar.tsx
+++ src/Components/Modules/Navbar.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import {
-  createStyles, ITheme, makeStyles, useMediaQuery
+  createStyles, ITheme, makeStyles
 } from "@chainsafe/common-theme";
 import { Grid, Typography, useHistory, Link } from "@chainsafe/common-components";
 
@@ -50,56 +50,6 @@ const useStyles = makeStyles(({ breakpoints, palette, zIndex, constants }: IThem
         color: "#e872dc",
       },
     },
-    menuToggle: {
-      [breakpoints.down(1170)]: {
-        display: "block",
-        cursor: "pointer",
-      },
-      display: "none",
-      width: "40px",
-      marginTop: "13px",
-      zIndex: 400000,
-      height: "40px",
-      transition: "transform 0.4s ease-in",
-    },
-    wrapper: {
-      background: palette.primary.main,
-      position: "absolute",
-      margin: "-1rem 0",
-      left: 0,
-      width: "100%",
-      height: "103vh",
-      zIndex: 400000,
-    },
-    mobileLinkContainer: {
-      zIndex: 1000,
-      display: "flex",
-      width: "100%",
-      marginTop: "15vh",
-      height: "auto",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    mobileLinkWrapper: {
-      marginTop: "2rem",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    mobileLink: {
-      textDecoration: "none",
-      fontSize: "3rem",
-      color: palette.common.white.main,
-      transition: "0.1s ease-in-out",
-      '&:hover': {
-        color: palette.common.white.main,
-      },
-      '&:active': {
-        color: palette.secondary.main,
-      },
-    },
   })
 })
 
@@ -108,15 +58,7 @@ const NavBar: React.FC = () => {
 
   const classes = useStyles();
 
-  const [isNavVisible, setIsNavVisible] = useState(false);
-
-  const isMobile = useMediaQuery("(max-width: 1170px)");
-
-  const toggleNav = () => {
-    setIsNavVisible(!isNavVisible);
-  }
   const { redirect } = useHistory()
-
 
   return (
     <nav className={classes.container}>
@@ -128,32 +70,10 @@ const NavBar: React.FC = () => {
             </Typography>
           </Grid>
         </Grid>
-        <div className={classes.menuToggle} onClick={toggleNav}>
-          <p>hello</p>
-
-        </div>
-
-        {!isMobile && <Grid item className={classes.linksMenu} >
-          <a href="https://twitter.com/tomoamiri" target="_blank" rel="noopener noreferrer" className={classes.link}>Twitter</a>
-          <a href="https://dribbble.com/tomoamiri" target="_blank" rel="noopener noreferrer" className={classes.link}>Dribble</a>
+        <Grid item className={classes.linksMenu} >
+          <a href="https://twitter.com/tomoamiri" target="_blank" rel="noopener noreferrer" className={classes.link}>About</a>
         </Grid>
-        }
       </Grid>
-      {(isMobile && isNavVisible) && (
-        <div className={classes.wrapper}>
-          <div className={classes.menuToggle} onClick={toggleNav} style={{ float: "right", margin: "1.7rem 1rem" }}>
-            <p>hello</p>
-          </div>
-          <ul className={classes.mobileLinkContainer}>
-            <div className={classes.mobileLinkWrapper} >
-              <a href="https://twitter.com/tomoamiri" target="_blank" rel="noopener noreferrer" className={classes.mobileLink}>Twitter</a>
-            </div>
-            <div className={classes.mobileLinkWrapper} >
-              <a href="https://dribbble.com/tomoamiri" target="_blank" rel="noopener noreferrer" className={classes.mobileLink}>Dribble</a>
-            </div>
-          </ul>
-        </div>
-      )}
     </nav >
   );
 };
