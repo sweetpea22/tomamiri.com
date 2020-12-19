c84875521a2f175127b570eaf8453b90580193c4
sweetpea22 <cindychau24@me.com>
2020-12-19 14:53:11 -0500

    Add kemistry detail page

--- src/Components/Pages/KemistryPage.tsx
+++ src/Components/Pages/KemistryPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from "../Modules/Navbar";
+import KemistryDetail from "../Subpages/KemistryDetail";
 
 const KemistryPage: React.FC = () => {
   return (
     <>
       <NavBar />
+      <KemistryDetail />
 
     </>
   )
--- src/Components/Subpages/KemistryDetail.tsx
+++ src/Components/Subpages/KemistryDetail.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Section from "../Modules/Section";
 import { createStyles, ITheme, makeStyles } from "@chainsafe/common-theme";
 import { Grid, Link, useHistory } from "@chainsafe/common-components";
-import fox from "./svg/fox.svg";
 
 const useStyles = makeStyles(({ breakpoints, palette, zIndex, constants }: ITheme) => {
 
@@ -65,19 +64,6 @@ const useStyles = makeStyles(({ breakpoints, palette, zIndex, constants }: IThem
       alignItems: "center",
       margin: "3rem 0",
     },
-    foxImg: {
-      position: "relative",
-      left: "50%",
-      transform: "translate(-50%)",
-      width: "20rem",
-    },
-    firstMockupImg: {
-      [breakpoints.up(1500)]: {
-        display: "block",
-      },
-      display: "none",
-    },
-
     bodyText: {
       fontSize: "28px",
       lineHeight: "38px",
@@ -119,11 +105,10 @@ const useStyles = makeStyles(({ breakpoints, palette, zIndex, constants }: IThem
     mockupContainer: {
       display: "flex",
       maxWidth: "100vw",
-      margin: "4rem 0",
-
+      margin: "4rem 0"
     },
     mockup: {
-      maxWidth: "30%",
+      maxWidth: "100%",
     },
     captionContainer: {
       display: "flex",
@@ -150,21 +135,11 @@ const useStyles = makeStyles(({ breakpoints, palette, zIndex, constants }: IThem
         }
       },
     },
-    finalConceptContainer: {
-      display: "flex",
-      maxWidth: "100vw",
-      margin: "4rem 0",
-      "& > img": {
-        [breakpoints.down('sm')]: {
-          maxWidth: "55%",
-          height: "500px",
-        }
-      },
-    },
+  
   })
 })
 
-const MetaDetail: React.FC = () => {
+const KemistryDetail: React.FC = () => {
   const classes = useStyles();
   const { redirect } = useHistory();
   return (
@@ -174,117 +149,71 @@ const MetaDetail: React.FC = () => {
           <Grid container className={classes.contentContainer}>
 
             <div>
-              <h1 className={classes.title}>Meta-alert</h1>
+              <h1 className={classes.title}>Kemistry</h1>
               <br></br>
-              <p className={classes.subtitleText}>Notifications for the Metamask wallet</p>
+              <p className={classes.subtitleText}>A collaborative audio workspace</p>
               <div className={classes.imgContainer}>
-                <img className={classes.firstMockupImg} src="/graphics/meta-alert/final1.png" alt="meta-alert prototype" />
-                <img src={fox} className={classes.foxImg} alt="meta-alert logo, notifications for metamask by thomas amiri" />
+                <img src="/graphics/kemistry/kemistry-screens.png" alt="meta-alert prototype" />
               </div>
             </div>
             <div>
-              <p className={classes.subtitleTextLight}>*This is concept work that is not affiliated with the official product</p>
+              <p className={classes.subtitleTextLight}>*This is concept work</p>
               <p className={classes.subHeading}>Summary</p>
               <p className={classes.bodyText}>
-                Wallets are key web3 portals for users and their functionality will only increase as they integrate more applications. For many of these apps the wallet is the only point of contact, meaning alerts of all kinds will have to exist. I set out to imagine some of these in the Metamask browser wallet.
+                The idea behind this concept is a collaborative interface (like google docs!) for musicians with features like version control and direct payments for selling samples and sounds. They could connect seamlessly with their web3 wallet. Also files could be stored inexpensively on decentralized storage networks like Filecoin.
               </p>
             </div>
             <div>
               <p className={classes.subHeading}>Context</p>
               <p className={classes.bodyText}>
-                Metamask recently hit its 1 millionth user milestone, cementing itself as one of blockchain’s most popular gateways. As a frequent user I’ve wished for certain notifications more than once. After doing some research I discovered more demand for alerts from every kind of user in the crypto space. I also got some sense of the Metamask roadmap and its big upcoming plugin system called Snaps.
+                A lot of activities have shifted online and musicians and other music lovers still want to jam and make music.
+              </p>
+              <p className={classes.bodyText}>
+                I found a similar app called Jammr that allows musicians to play simultaneously over the web. Although Jammr focuses on live perfomance, it’s similar in the way that it connects artists realtime. The app gained a lot of popularity during the beginning of the pandemic since there was a lot of demand for virtual gatherings and an absence of music oriented platforms.
               </p>
             </div>
             <div>
               <p className={classes.subHeading}>Challenges</p>
               <p className={classes.bodyText}>
-                The wallet is regularly processing transactions and interacting with all kinds of dapps but this activity is rarely communicated except in network logs. User’s are left wondering about the status of transactions and other web3 events.
+                Audio workstations are often complex with many robust features. Recording and composing song elements in real-time with others could easily overwhelm users. I needed to do some research and narrow the app’s scope.
               </p>
             </div>
             <div>
               <p className={classes.subHeading}>The Design Process</p>
               <p className={classes.bodyText}>
-                I got a lot of insight from the feature discussions on the Metamask Github and was sure I wanted to keep my design aligned with the Snap plugin roadmap. This helped me differentiate between two types of alerts: system alerts and plugin alerts. The former were global to all the wallets in your Metamask, while the latter were local to a specific address.
-              </p>
-              <p className={classes.bodyText}>
-                Next, I leveraged some well-documented Ethereum personas and created user journeys for three types of users: an artist, investor, and socializer. The user journey’s really emphasized to me the breadth of needs that needed to be accommodated in the interface. They also highlighted key opportunities for integrating alerts. I then mapped out task flows to exactly pinpoint where and when alerts should interact with the user.
+                I thought it would be key to get a better sense of the target audience and define the scope of features. Luckily I was able to interview two friends, one a professional musician and another who played for fun. I conducted the user interviews and avoided leading questions. I focused more on asking about memories and examples of times they collaborated with others. I then pitched my paper MVP and got some suggestions for the UI. A key insight was to keep the features of the workstation general so that it would be more accessible to a greater spectrum of makers. They were also enthusiastic about the idea of collaborating with musicians from all over the world.
               </p>
             </div>
             <div className={classes.mockupContainer}>
-              <img src="/graphics/meta-alert/user-journey1.png" className={classes.mockup} alt="" />
-              <img src="/graphics/meta-alert/user-journey2.png" className={classes.mockup} alt="" />
-              <img src="/graphics/meta-alert/user-journey3.png" className={classes.mockup} alt="" />
+              <img src="/graphics/kemistry/interview-notes.png" className={classes.mockup} alt="" />
             </div>
             <div>
-              <img src="/graphics/meta-alert/task-flows.png" className={classes.mockup} alt="" />
-            </div>
-            <div className={classes.mockupContainer}>
-              <img src="/graphics/meta-alert/taskflow1.png" className={classes.mockup} alt="" />
-              <img src="/graphics/meta-alert/taskflow2.png" className={classes.mockup} alt="" />
-              <img src="/graphics/meta-alert/taskflow3.png" className={classes.mockup} alt="" />
-            </div>
-            <div>
-              <p className={classes.subHeading}>Wireframes</p>
               <p className={classes.bodyText}>
-                Once I established the context of alerts and their structure I hopped into wire framing. From paper sketches I worked into high fidelity mock ups. Metamask is already very compact as a browser plugin so it was a challenge to find an optimal layout without cluttering the UI or overwhelming the user.
+                I knew from the interviews to keep the interface simple. Musicians should be able to quickly connect to the workstation, invite other creators, and compile sounds together with minimal friction. I began mapping out possible user flows quickly on paper until I had a basic set of requirements that would guide my designs. From there I developed more high-fidelity mock ups and started to finalize the layout.
               </p>
-            </div>
-            <div className={classes.mockupContainer}>
-              <img src="/graphics/meta-alert/wireframes.png" alt="" />
             </div>
             <div>
               <p className={classes.subHeading}>Final Concept</p>
-              <p className={classes.bodyText}>
-                I asked a friend who was also a Metamask user to test my prototype. From this interview I learned that how I categorized the tabs was confusing and that some of the new elements were unintuitive. So I revisited my wireframes and focused on keep the existing UI more intact while creating a better sense of separation between functions.
-              </p>
             </div>
-            <div className={classes.finalConceptContainer}>
-              <img src="/graphics/meta-alert/final1.png" alt="Prototype of Metamask notifications by Thomas Amiri" />
-              <div className={classes.captionContainer}>
-                <p className={classes.subtitleTextLight}>
-                  Flip between your funds and your plugins
-              </p>
-                <div></div>
-                <p className={classes.subtitleTextLight}>
-                  Know when your wallet processes a transaction
-              </p>
-                <div></div>
-              </div>
-              <img src="/graphics/meta-alert/final2.png" alt="Prototype of Metamask notifications by Thomas Amiri" />
-              <div className={classes.captionContainer}>
-                <p className={classes.subtitleTextLight}>
-                  Be notified of your other persona’s activity
-              </p>
-                <div></div>
-              </div>
+            <div className={classes.mockupContainer}>
+              <img src="/graphics/kemistry/mock1.png" className={classes.mockup} alt="" />
             </div>
-            <div className={classes.finalConceptContainer}>
-              <img src="/graphics/meta-alert/final3.png" alt="Prototype of Metamask notifications by Thomas Amiri" />
-              <div className={classes.captionContainer}>
-                <p className={classes.subtitleTextLight}>
-                  Conveniently view dapp
-                  communications
-              </p>
-                <div></div>
-              </div>
-              <img src="/graphics/meta-alert/final4.png" alt="Prototype of Metamask notifications by Thomas Amiri" />
-              <div className={classes.captionContainer}>
-                <p className={classes.subtitleTextLight}>
-                  Control what dapps connect to your wallet and manage their permissions
-              </p>
-                <div></div>
-              </div>
+            <div className={classes.mockupContainer}>
+              <img src="/graphics/kemistry/secondlast.png" className={classes.mockup} alt="" />
+            </div>
+            <div className={classes.mockupContainer}>
+              <img src="/graphics/kemistry/lastmock.png" className={classes.mockup} alt="" />
+            </div>
+            <div className={classes.linkWrapper}>
+              <Link onClick={() => redirect('/criterion')}>
+                <p className={classes.link}>Check out next project &gt;</p>
+              </Link>
             </div>
           </Grid>
-          <div className={classes.linkWrapper}>
-            <Link onClick={() => redirect('/kemistry')}>
-              <p className={classes.link}>Check out next project &gt;</p>
-            </Link>
-          </div>
         </div>
       </Section>
     </>
   )
 }
 
-export default MetaDetail
+export default KemistryDetail
